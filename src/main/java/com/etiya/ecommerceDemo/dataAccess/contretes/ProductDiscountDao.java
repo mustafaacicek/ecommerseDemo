b6a312fa4ea0c753a2374bc3715224ad59429fa3
mustafa <mustafaciceknote8@gmail.com>
2023-04-25 12:45:40 +0300

    ProductDiscountDao

--- src/main/java/com/etiya/ecommerceDemo/dataAccess/contretes/ProductDiscountDao.java
+++ src/main/java/com/etiya/ecommerceDemo/dataAccess/contretes/ProductDiscountDao.java
@@ -1,7 +1,18 @@
 package com.etiya.ecommerceDemo.dataAccess.contretes;
 
+import com.etiya.ecommerceDemo.entities.concrete.Product;
 import com.etiya.ecommerceDemo.entities.concrete.ProductDiscount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProductDiscountDao extends JpaRepository<ProductDiscount, Long> {
+    @Query(value = "select * from public.product_discount")
+    List<Product>getProductDiscount();
+
+    @Query(value = "SELECT * FROM public.product_discount pd where pd.discount_amount = (select MAX(pd2.discount_amount) FROM public.product_discount pd2)")
+    List<Product>getMaxProductDiscount();
+
+
 }
